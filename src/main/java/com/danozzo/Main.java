7e179f4f5c498c1dc6d7e5fbb293820b1f2aa4f8
Daniele410 <miraglia.daniele410@gmail.com>
2025-02-10 20:12:05 +0100

    Fix main class

--- src/main/java/com/danozzo/Main.java
+++ src/main/java/com/danozzo/Main.java
@@ -3,6 +3,17 @@ package com.danozzo;
 import javax.swing.*;
 
 public class Main extends JFrame {
+
+    public Main() {
+        add(new Model());
+    }
+
     public static void main(String[] args) {
+        Main pac = new Main();
+        pac.setVisible(true);
+        pac.setTitle("Pacman");
+        pac.setSize(380,420);
+        pac.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        pac.setLocationRelativeTo(null);
     }
 }
